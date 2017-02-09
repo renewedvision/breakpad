aea5e2787f87f7f7851635420cff368bd4fe4782
Gabriele Svelto <gsvelto@mozilla.com>
2017-02-09 14:40:12 +0100

    Fix minidump name generation on Linux
    This patch changes minidump filename generation on Linux so that the
    name is in the same 8-4-4-4-12.dmp format as on the other platforms
    and ensures that two crashes taken within the same second have different
    minidump names. To fix the latter issue the random characters used in
    the minidump filename are now read from /dev/urandom were possible or
    generated via arc4random(). If neither is available we fall back to
    regular rand() but mixing the address of an object to the current time
    when generating the random seed to make it slightly less predictable.
    
    BUG=727
    
    Change-Id: I4d0d6b6444de246b534003242ed50cb5bc780984

--- src/common/linux/guid_creator.cc
+++ src/common/linux/guid_creator.cc
@@ -36,6 +36,11 @@
 #include <time.h>
 #include <unistd.h>
 
+#if !defined(HAVE_ARC4RANDOM)
+#include <sys/stat.h>
+#include <fcntl.h>
+#endif
+
 //
 // GUIDGenerator
 //
@@ -46,43 +51,101 @@
 //
 class GUIDGenerator {
  public:
-  static uint32_t BytesToUInt32(const uint8_t bytes[]) {
-    return ((uint32_t) bytes[0]
-            | ((uint32_t) bytes[1] << 8)
-            | ((uint32_t) bytes[2] << 16)
-            | ((uint32_t) bytes[3] << 24));
+  static uint16_t BytesToUInt16(const uint8_t bytes[]) {
+    return ((uint16_t) bytes[1] << 8) | ((uint16_t) bytes[0]);
   }
 
-  static void UInt32ToBytes(uint8_t bytes[], uint32_t n) {
-    bytes[0] = n & 0xff;
-    bytes[1] = (n >> 8) & 0xff;
-    bytes[2] = (n >> 16) & 0xff;
-    bytes[3] = (n >> 24) & 0xff;
+  // The last field in a GUID is 48 bits long so we're converting only 6 bytes
+  static uint64_t BytesToUInt48(const uint8_t bytes[]) {
+    return ((uint64_t) bytes[0] << 40) | ((uint64_t) bytes[1] << 32) |
+           ((uint64_t) bytes[2] << 24) | ((uint64_t) bytes[3] << 16) |
+           ((uint64_t) bytes[4] << 8)  | (uint64_t) bytes[5];
   }
 
   static bool CreateGUID(GUID *guid) {
-    InitOnce();
-    guid->data1 = random();
-    guid->data2 = (uint16_t)(random());
-    guid->data3 = (uint16_t)(random());
-    UInt32ToBytes(&guid->data4[0], random());
-    UInt32ToBytes(&guid->data4[4], random());
+#if defined(HAVE_ARC4RANDOM) // Android, BSD, ...
+    CreateGuidFromArc4Random(guid);
+#else // Linux
+    if (!CreateGUIDFromDevUrandom(guid)) {
+      CreateGUIDFromRand(guid);
+    }
+#endif
+
+    /* Put in the version */
+    guid->data3 &= 0x0fff;
+    guid->data3 |= 0x4000;
+
+    /* Put in the variant */
+    guid->data4[0] &= 0x3f;
+    guid->data4[0] |= 0x80;
+
     return true;
   }
 
  private:
+#ifdef HAVE_ARC4RANDOM
+  static void CreateGuidFromArc4Random(GUID *guid) {
+    char *buff = (char *)guid;
+
+    for (size_t i = 0; i < sizeof(GUID); i += sizeof(uint32_t)) {
+      uint32_t random_data = arc4random();
+
+      memcpy(buff + i, &random_data, sizeof(uint32_t));
+    }
+  }
+#else
   static void InitOnce() {
     pthread_once(&once_control, &InitOnceImpl);
   }
 
   static void InitOnceImpl() {
-    srandom(time(NULL));
+    // time(NULL) is a very poor seed, so lacking anything better mix an
+    // address into it.
+    srand(time(NULL) | (((uintptr_t)&once_control) >> 4));
   }
 
   static pthread_once_t once_control;
+
+  // Populate the GUID using random bytes read from /dev/urandom, returns false
+  // if the GUID wasn't fully populated with random data
+  static bool CreateGUIDFromDevUrandom(GUID *guid) {
+    size_t read_bytes = 0;
+    char *buff = (char *)guid;
+    int fd = open("/dev/urandom", O_RDONLY);
+
+    if (fd != -1) {
+      do {
+        int res = read(fd, buff + read_bytes, sizeof(GUID) - read_bytes);
+
+        if (res < 0) {
+          break;
+        }
+
+        read_bytes += res;
+      } while (read_bytes < sizeof(GUID));
+
+      close(fd);
+    }
+
+    return (read_bytes == sizeof(GUID));
+  }
+
+  // Populate the GUID using a stream of random bytes obtained from rand()
+  static void CreateGUIDFromRand(GUID *guid) {
+    char *buff = (char *)guid;
+
+    InitOnce();
+
+    for (size_t i = 0; i < sizeof(GUID); i++) {
+      buff[i] = rand();
+    }
+  }
+#endif
 };
 
+#ifndef HAVE_ARC4RANDOM
 pthread_once_t GUIDGenerator::once_control = PTHREAD_ONCE_INIT;
+#endif
 
 bool CreateGUID(GUID *guid) {
   return GUIDGenerator::CreateGUID(guid);
@@ -94,8 +157,8 @@ bool GUIDToString(const GUID *guid, char *buf, int buf_len) {
   assert(buf_len > kGUIDStringLength);
   int num = snprintf(buf, buf_len, kGUIDFormatString,
                      guid->data1, guid->data2, guid->data3,
-                     GUIDGenerator::BytesToUInt32(&(guid->data4[0])),
-                     GUIDGenerator::BytesToUInt32(&(guid->data4[4])));
+                     GUIDGenerator::BytesToUInt16(&(guid->data4[0])),
+                     GUIDGenerator::BytesToUInt48(&(guid->data4[2])));
   if (num != kGUIDStringLength)
     return false;
 
