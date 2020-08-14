6928667020401bcb0e12429082e8418ca815bd20
Sim Sun <sunsj1231@gmail.com>
2020-08-14 03:12:01 -0700

    linux: Fix segfault when there is no section header in ELF file
    Some vendor library doesn't contain the section header. It causes
    segmentation fault in `FindElfClassSection`.
    
    > e_shoff:
    > This member holds the section header table's file offset in bytes.
    > If the file has no section header table this member holds zero.
    
    Change-Id: Id98d6ff3bd16af4541deb5a55a8fad2fa74eda23

--- src/common/linux/elfutils.cc
+++ src/common/linux/elfutils.cc
@@ -57,6 +57,11 @@ void FindElfClassSection(const char* elf_base,
   const Ehdr* elf_header = reinterpret_cast<const Ehdr*>(elf_base);
   assert(elf_header->e_ident[EI_CLASS] == ElfClass::kClass);
 
+  if (elf_header->e_shoff == 0) {
+    // e_shoff holds zero if the file has no section header table.
+    return;
+  }
+
   const Shdr* sections =
     GetOffset<ElfClass, Shdr>(elf_header, elf_header->e_shoff);
   const Shdr* section_names = sections + elf_header->e_shstrndx;
