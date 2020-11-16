5a7844a51b7bf2c1f12228a575c27fbc92e745b2
Ludovic Guegan <ludovic.guegan@gmail.com>
2020-11-16 14:30:58 +0100

    core_handler: coredump handler to produce minidump
    On Linux, it is possible to register a core handler via
    /proc/sys/kernel/core_pattern. Doing so invokes the core handler when
    a process crash. The core_handler uses /proc/<pid>/mem to access the
    process memory. This way it is not necessary to process the full
    coredump which takes time and consumes memory.
    
    In order to profit from this core handler, for example, one can
    integrate dump_syms into Yocto and generate an archive with the
    breakpad symbols of all the binaries in the rootfs. Minidumps are
    especially useful on embedded systems since they are lightweight and
    provide contextual information.
    
    Bug: This development is motivated by the need to capture the system
    logs (and other information) into the minidump. Because of the way
    minidumps are generated on Linux, it is not possible to use external
    libraries during the generation of the dump (like sd-journal(3)).
    A true out-of-process mechanism is needed.
    
    Change-Id: I9298d81159029cefb81c915831db54884310ad05

--- src/common/linux/elf_core_dump.h
+++ src/common/linux/elf_core_dump.h
@@ -139,9 +139,15 @@ class ElfCoreDump {
   // an empty note if no note is found.
   Note GetFirstNote() const;
 
+  // Sets the mem fd.
+  void SetProcMem(const int fd);
+
  private:
   // Core dump content.
   MemoryRange content_;
+
+  // descriptor for /proc/<pid>/mem
+  int proc_mem_fd_;
 };
 
 }  // namespace google_breakpad
---
+++ src/tools/linux/core_handler/core_handler.cc
@@ -0,0 +1,157 @@
+// Copyright (c) 2020, Google Inc.
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are
+// met:
+//
+//     * Redistributions of source code must retain the above copyright
+// notice, this list of conditions and the following disclaimer.
+//     * Redistributions in binary form must reproduce the above
+// copyright notice, this list of conditions and the following disclaimer
+// in the documentation and/or other materials provided with the
+// distribution.
+//     * Neither the name of Google Inc. nor the names of its
+// contributors may be used to endorse or promote products derived from
+// this software without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+// "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+// LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+// A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+// OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+// SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+// LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+// DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+// THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+// (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+// OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+// core_handler.cc: An utility to handle coredumps on Linux
+
+#include <pwd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/mman.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <sstream>
+
+#include "client/linux/minidump_writer/minidump_writer.h"
+#include "client/linux/minidump_writer/linux_core_dumper.h"
+
+using google_breakpad::AppMemoryList;
+using google_breakpad::MappingList;
+using google_breakpad::LinuxCoreDumper;
+
+void ShowUsage(const char* argv0) {
+  fprintf(stderr, "Usage: %s <pid> <minidump output>\n", argv0);
+}
+
+bool WriteMinidumpFromCore(const char* filename,
+    const char* core_path,
+    const char* procfs_override) {
+  MappingList mappings;
+  AppMemoryList memory_list;
+  LinuxCoreDumper dumper(0, core_path, procfs_override);
+  return google_breakpad::WriteMinidump(filename, mappings, memory_list,
+      &dumper);
+}
+
+bool HandleCrash(pid_t pid, const char* procfs_dir, const char* md_filename) {
+  /* Only reads the first mega byte from stdin and save it in a file in order
+   * to force the dumper to fallback to /proc/<pid>/mem */
+  const int buf_size = 1024*1024;
+  char *buf = reinterpret_cast<char*>(malloc(buf_size));
+  if (buf == NULL) {
+      fprintf(stderr, "failed to allocate coredump buffer\n");
+      return false;
+  }
+  int r = 0;
+  while (r != buf_size) {
+    int ret = read(STDIN_FILENO, buf + r, buf_size - r);
+    if (ret == 0) {
+      break;
+    } else if (ret == -1) {
+      fprintf(stderr, "failed to read core (%d/%d, %d)\n", r, buf_size, errno);
+      free(buf);
+      return false;
+    }
+    r += ret;
+  }
+
+  int fd = memfd_create("core_file", 0);
+  if (fd == -1) {
+    fprintf(stderr, "failed to create core file (%d)\n", errno);
+    free(buf);
+    return false;
+  }
+
+  int w = write(fd, buf, r);
+  if (w != r) {
+    fprintf(stderr, "failed to write core dump (%d/%d, %d)\n", w, r, errno);
+    close(fd);
+    free(buf);
+    return false;
+  }
+  free(buf);
+
+  std::stringstream core_file_ss;
+  core_file_ss << "/proc/self/fd/" << fd;
+  const char *core_file = core_file_ss.str().c_str();
+
+  if (!WriteMinidumpFromCore(md_filename, core_file, procfs_dir)) {
+    fprintf(stderr, "Unable to generate minidump.\n");
+    close(fd);
+    return false;
+  }
+  close(fd);
+
+  return true;
+}
+
+
+int main(int argc, char *argv[]) {
+  int ret = EXIT_FAILURE;
+
+  if (argc < 1) {
+    return ret;
+  } else if (argc != 3) {
+    ShowUsage(argv[0]);
+    return ret;
+  }
+
+  const char* pid_str = argv[1];
+  const char* md_filename = argv[2];
+  pid_t pid = atoi(pid_str);
+
+  FILE *fl = popen("/usr/bin/logger -t core_handler", "w");
+  if (fl == NULL) {
+    fprintf(stderr, "failed to open logger\n");
+    return ret;
+  }
+
+  /* redirect stdout and stderr to the logger */
+  int nf = fileno(fl);
+  dup2(nf, STDOUT_FILENO);
+  dup2(nf, STDERR_FILENO);
+
+  std::stringstream proc_dir_ss;
+  proc_dir_ss << "/proc/" << pid_str;
+  const char *proc_dir = proc_dir_ss.str().c_str();
+
+  if (HandleCrash(pid, proc_dir, md_filename)) {
+    fprintf(stderr, "minidump generated at %s\n", md_filename);
+    ret = EXIT_SUCCESS;
+  } else {
+    fprintf(stderr, "cannot generate minidump %s\n", md_filename);
+  }
+
+  fflush(stdout);
+  fflush(stderr);
+  fclose(stdout);
+  fclose(stderr);
+  pclose(fl);
+
+  return ret;
+}
