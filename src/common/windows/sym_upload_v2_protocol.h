503ae80ab972f8cfbea07aa9cd64bf4f96e346d8
Nelson Billing <nbilling@google.com>
2022-05-19 15:04:34 -0700

    Make sym-upload-v2 windows code shareable.
    Change-Id: I228c93655203977b27052a85705c42bafef1e1ef

---
+++ src/common/windows/sym_upload_v2_protocol.h
@@ -0,0 +1,23 @@
+#ifndef COMMON_WINDOWS_SYM_UPLOAD_V2_PROTOCOL_H_
+#define COMMON_WINDOWS_SYM_UPLOAD_V2_PROTOCOL_H_
+
+#include <string>
+
+namespace google_breakpad {
+
+// Sends file at |symbol_filename| using the sym-upload-v2 protocol to
+// |api_url| using key |api_key|, and using identifiers |debug_file| and
+// |debug_id|. |timeout_ms| is the number of milliseconds to wait before
+// terminating the upload attempt. If |force| is set then it will overwrite an
+// existing file with the same |debug_file| and |debug_id| in the store.
+bool SymUploadV2ProtocolSend(const wchar_t* api_url,
+                             const wchar_t* api_key,
+                             int* timeout_ms,
+                             const std::wstring& debug_file,
+                             const std::wstring& debug_id,
+                             const std::wstring& symbol_filename,
+                             bool force);
+
+}  // namespace google_breakpad
+
+#endif  // COMMON_WINDOWS_SYM_UPLOAD_V2_PROTOCOL_H_
