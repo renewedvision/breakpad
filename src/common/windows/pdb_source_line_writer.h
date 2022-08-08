0da8bb58c2c91aca0f9f2c0449bdeed123daf9be
Lorenzo Alberto Maria Ambrosi <lorenzothunder.ambrosi@gmail.com>
2022-08-08 16:05:54 +0200

    Add error messages on pdb Open function
    This implements the error messages from
    https://docs.microsoft.com/en-us/visualstudio/debugger/debug-interface-access/idiadatasource-loaddataforexe?view=vs-2022
    and
    https://docs.microsoft.com/en-us/visualstudio/debugger/debug-interface-access/idiadatasource-loaddatafrompdb?view=vs-2022
    instead of having just a generic error message.
    
    Signed-off-by: Lorenzo Alberto Maria Ambrosi <lorenzothunder.ambrosi@gmail.com>
    Bug: https://bugs.chromium.org/p/google-breakpad/issues/detail?id=866
    Change-Id: I23c0e80d31afb402a70cb0cdded78d3d34ac5fff

--- src/common/windows/pdb_source_line_writer.h
+++ src/common/windows/pdb_source_line_writer.h
@@ -66,6 +66,7 @@ class PDBSourceLineWriter {
   explicit PDBSourceLineWriter(bool handle_inline = false);
   ~PDBSourceLineWriter();
 
+
   // Opens the given file.  For executable files, the corresponding pdb
   // file must be available; Open will be if it is not.
   // If there is already a pdb file open, it is automatically closed.
