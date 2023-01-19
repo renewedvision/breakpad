fb21d350e250b4fd4dab89e654781c7b1d80e957
Leonard Grey <lgrey@chromium.org>
2023-01-19 12:21:50 -0500

    Mac: add shell script to coordinate dumping and uploading macOS system symbols.
    This checks in an edited version of a script that has been used by the
    Chrome Mac team for this purpose. It expects to reside in the same
    place as `dump_syms`, `dsc_extractor`[0], `symupload` and `upload_system_symbols` binaries.
    
    When called, it will:
    - Locate and extract any dyld_shared_caches found on the system
    - Dump the above
    - Dump any remaining uncached system libraries
    - Write the results to a directory passed as an argument
    - Provide (but not call) an `upload_system_symbols` invocation
    to upload the results
    
    [0] Not yet checked in
    
    Bug: 1400770
    
    Change-Id: I30610c23d0c979e34dd3830eeedb5ceeae8ce66b

---
+++ src/tools/mac/upload_system_symbols/upload_system_symbols.sh
@@ -0,0 +1,106 @@
+#!/bin/bash
+
+# Copyright 2023 Google LLC
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are
+# met:
+#
+#     * Redistributions of source code must retain the above copyright
+# notice, this list of conditions and the following disclaimer.
+#     * Redistributions in binary form must reproduce the above
+# copyright notice, this list of conditions and the following disclaimer
+# in the documentation and/or other materials provided with the
+# distribution.
+#     * Neither the name of Google LLC nor the names of its
+# contributors may be used to endorse or promote products derived from
+# this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+# A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+# OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+# SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+# LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+# DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+# THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+# Finds the dyld_shared_cache on a system, extracts it, and dumps the symbols
+# in Breakpad format to the directory passed as the first argument
+# The script must be in the same directory as `dump_syms`, 
+# `upload_system_symbols` and `dsc_extractor` binaries.
+# Exits with 0 if all supported architectures for this OS version were found and
+# dumped, and nonzero otherwise.
+
+set -ex
+
+if [[ $# -ne 1 ]]; then
+  echo "Usage $0 <destination_directory>"
+  exit 1
+fi
+
+destination_dir="$1"
+
+dir="$(dirname "$0")"
+dir="$(cd "${dir}"; pwd)"
+major_version=$(sw_vers -productVersion | cut -d . -f 1)
+if [[ "${major_version}" -lt 13 ]]; then
+  dsc_directory="/System/Library/dyld"
+else
+  dsc_directory="/System/Volumes/Preboot/Cryptexes/OS/System/Library/dyld"
+fi
+version="$(sw_vers -productVersion).$(sw_vers -buildVersion)"
+
+working_dir=$(mktemp -d)
+trap 'rm -rf "${working_dir}"' EXIT
+mkdir "${working_dir}/dsc.${version}" "${destination_dir}/breakpad.${version}"
+
+architectures=(x86_64h)
+missing_architectures=()
+if [[ "${major_version}" -lt 13 ]]; then
+  architectures+=( x86_64 )
+fi
+if [[ "${major_version}" -ge 11 ]]; then
+  architectures+=( arm64e )
+fi
+
+for arch in "${architectures[@]}"; do
+  cache="${dsc_directory}/dyld_shared_cache_${arch}"
+  if [[ ! -f "${cache}" ]]; then
+    missing_architectures+=("${arch}")
+    continue
+  fi
+  "${dir}/dsc_extractor" \
+      "${cache}" \
+      "${working_dir}/dsc.${version}/${arch}"
+  "${dir}/upload_system_symbols" \
+      --breakpad-tools="${dir}" \
+      --system-root="${working_dir}/dsc.${version}/${arch}" \
+      --dump-to="${destination_dir}/breakpad.${version}"
+done
+"${dir}/upload_system_symbols" \
+    --breakpad-tools="${dir}" \
+    --system-root=/ \
+    --dump-to="${destination_dir}/breakpad.${version}"
+
+set +x
+echo
+echo "Dumped!"
+echo "To upload, run:"
+echo
+echo "'${dir}/upload_system_symbols'" \\
+echo "    --breakpad-tools='${dir}'" \\
+echo "    --api-key=<YOUR API KEY>" \\
+echo "    --upload-from='${destination_dir}/breakpad.${version}'"
+
+if [[ "${#missing_architectures[@]}" -gt 0 ]]; then
+  echo "dyld_shared_cache not found for architecture(s):" >& 2
+  echo "  " "${missing_architectures[@]}" >& 2
+  echo "You'll need to get symbols for them elsewhere." >& 2
+  exit 1
+else
+  exit 0
+fi
