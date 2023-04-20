88865c7579f823ec4e0d4892e106671385ec6e0d
Leonard Grey <lgrey@chromium.org>
2023-04-20 14:42:13 -0400

    Update Mac headers
    This is primarily to let us remove some defines and
    to make the Mac-on-Linux dump_syms arm64-aware.
    
    As in the original import, things are cut down, but
    not maximally so. "Interesting" changes include:
    - Swapping `defined(__arm64__)` for `defined(__aarch64__)`
    - Removing `__kernel_ptr_semantics` tags
    - No-oping deprecation notices in arch.h (hopefully,
    the next CL will let us remove this entirely)
    
    Bug: chromium:1420654, google-breakpad:880
    Change-Id: Ia807a68884be0cbc2bf808ac473e66efb3b5ecc9

--- src/third_party/mac_headers/mach/i386/vm_param.h
+++ src/third_party/mac_headers/mach/i386/vm_param.h
@@ -1,8 +1,8 @@
 /*
- * Copyright (c) 2000-2006 Apple Computer, Inc. All rights reserved.
+ * Copyright (c) 2000-2012 Apple Inc. All rights reserved.
  *
  * @APPLE_OSREFERENCE_LICENSE_HEADER_START@
- * 
+ *
  * This file contains Original Code and/or Modifications of Original Code
  * as defined in and that are subject to the Apple Public Source License
  * Version 2.0 (the 'License'). You may not use this file except in
@@ -11,10 +11,10 @@
  * unlawful or unlicensed copies of an Apple operating system, or to
  * circumvent, violate, or enable the circumvention or violation of, any
  * terms of an Apple operating system software license agreement.
- * 
+ *
  * Please obtain a copy of the License at
  * http://www.opensource.apple.com/apsl/ and read it before using this file.
- * 
+ *
  * The Original Code and all software distributed under the License are
  * distributed on an 'AS IS' basis, WITHOUT WARRANTY OF ANY KIND, EITHER
  * EXPRESS OR IMPLIED, AND APPLE HEREBY DISCLAIMS ALL SUCH WARRANTIES,
@@ -22,39 +22,39 @@
  * FITNESS FOR A PARTICULAR PURPOSE, QUIET ENJOYMENT OR NON-INFRINGEMENT.
  * Please see the License for the specific language governing rights and
  * limitations under the License.
- * 
+ *
  * @APPLE_OSREFERENCE_LICENSE_HEADER_END@
  */
 /*
  * @OSF_COPYRIGHT@
  */
-/* 
+/*
  * Mach Operating System
  * Copyright (c) 1991,1990,1989,1988 Carnegie Mellon University
  * All Rights Reserved.
- * 
+ *
  * Permission to use, copy, modify and distribute this software and its
  * documentation is hereby granted, provided that both the copyright
  * notice and this permission notice appear in all copies of the
  * software, derivative works or modified versions, and any portions
  * thereof, and that both notices appear in supporting documentation.
- * 
+ *
  * CARNEGIE MELLON ALLOWS FREE USE OF THIS SOFTWARE IN ITS "AS IS"
  * CONDITION.  CARNEGIE MELLON DISCLAIMS ANY LIABILITY OF ANY KIND FOR
  * ANY DAMAGES WHATSOEVER RESULTING FROM THE USE OF THIS SOFTWARE.
- * 
+ *
  * Carnegie Mellon requests users of this software to return to
- * 
+ *
  *  Software Distribution Coordinator  or  Software.Distribution@CS.CMU.EDU
  *  School of Computer Science
  *  Carnegie Mellon University
  *  Pittsburgh PA 15213-3890
- * 
+ *
  * any improvements or extensions that they make and grant Carnegie Mellon
  * the rights to redistribute these changes.
  */
 
-/* 
+/*
  * Copyright (c) 1994 The University of Utah and
  * the Computer Systems Laboratory at the University of Utah (CSL).
  * All rights reserved.
@@ -87,56 +87,37 @@
  *	them.
  */
 
-#ifndef	_MACH_I386_VM_PARAM_H_
+#ifndef _MACH_I386_VM_PARAM_H_
 #define _MACH_I386_VM_PARAM_H_
 
-#define BYTE_SIZE		8		/* byte size in bits */
+#if defined (__i386__) || defined (__x86_64__)
 
-#define I386_PGBYTES		4096		/* bytes per 80386 page */
-#define I386_PGSHIFT		12		/* bitshift for pages */
+#define BYTE_SIZE               8               /* byte size in bits */
 
-#define	PAGE_SIZE		I386_PGBYTES
-#define	PAGE_SHIFT		I386_PGSHIFT
-#define	PAGE_MASK		(PAGE_SIZE - 1)
+#define I386_PGBYTES            4096            /* bytes per 80386 page */
+#define I386_PGSHIFT            12              /* bitshift for pages */
 
-#define I386_LPGBYTES		2*1024*1024	/* bytes per large page */
-#define I386_LPGSHIFT		21		/* bitshift for large pages */
-#define I386_LPGMASK		(I386_LPGBYTES-1)
+
+
+#define VM_MIN_ADDRESS64        ((user_addr_t) 0x0000000000000000ULL)
+/*
+ * Default top of user stack, grows down from here.
+ * Address chosen to be 1G (3rd level page table entry) below SHARED_REGION_BASE_X86_64
+ * minus additional 1Meg (1/2 1st level page table coverage) to allow a redzone after it.
+ */
+#define VM_USRSTACK64           ((user_addr_t) (0x00007FF7C0000000ull - (1024 * 1024)))
 
 /*
- *	Convert bytes to pages and convert pages to bytes.
- *	No rounding is used.
+ * XXX TODO: Obsolete?
  */
-
-#define i386_btop(x)		((ppnum_t)((x) >> I386_PGSHIFT))
-#define machine_btop(x)		i386_btop(x)
-#define i386_ptob(x)		(((pmap_paddr_t)(x)) << I386_PGSHIFT)
-
-/*
- *	Round off or truncate to the nearest page.  These will work
- *	for either addresses or counts.  (i.e. 1 byte rounds to 1 page
- *	bytes.
- */
-
-#define i386_round_page(x)	((((pmap_paddr_t)(x)) + I386_PGBYTES - 1) & \
-					~(I386_PGBYTES-1))
-#define i386_trunc_page(x)	(((pmap_paddr_t)(x)) & ~(I386_PGBYTES-1))
-
-
-
-#define VM_MIN_ADDRESS64	((user_addr_t) 0x0000000000000000ULL)
-/*
- * default top of user stack... it grows down from here
- */
-#define VM_USRSTACK64		((user_addr_t) 0x00007FFF5FC00000ULL)
-#define VM_DYLD64		((user_addr_t) 0x00007FFF5FC00000ULL)
-#define VM_LIB64_SHR_DATA	((user_addr_t) 0x00007FFF60000000ULL)
-#define VM_LIB64_SHR_TEXT	((user_addr_t) 0x00007FFF80000000ULL)
+#define VM_DYLD64               ((user_addr_t) 0x00007FFF5FC00000ULL)
+#define VM_LIB64_SHR_DATA       ((user_addr_t) 0x00007FFF60000000ULL)
+#define VM_LIB64_SHR_TEXT       ((user_addr_t) 0x00007FFF80000000ULL)
 /*
  * the end of the usable user address space , for now about 47 bits.
  * the 64 bit commpage is past the end of this
  */
-#define VM_MAX_PAGE_ADDRESS	((user_addr_t) 0x00007FFFFFE00000ULL)
+#define VM_MAX_PAGE_ADDRESS     ((user_addr_t) 0x00007FFFFFE00000ULL)
 /*
  * canonical end of user address space for limits checking
  */
@@ -144,14 +125,16 @@
 
 
 /* system-wide values */
-#define MACH_VM_MIN_ADDRESS		((mach_vm_offset_t) 0)
-#define MACH_VM_MAX_ADDRESS		((mach_vm_offset_t) VM_MAX_PAGE_ADDRESS)
+#define MACH_VM_MIN_ADDRESS             ((mach_vm_offset_t) 0)
+#define MACH_VM_MAX_ADDRESS             ((mach_vm_offset_t) VM_MAX_PAGE_ADDRESS)
 
 /* process-relative values (all 32-bit legacy only for now) */
-#define VM_MIN_ADDRESS		((vm_offset_t) 0)
-#define VM_USRSTACK32		((vm_offset_t) 0xC0000000)
-#define VM_MAX_ADDRESS		((vm_offset_t) 0xFFE00000)
+#define VM_MIN_ADDRESS          ((vm_offset_t) 0)
+#define VM_USRSTACK32           ((vm_offset_t) 0xC0000000)      /* ASLR slides stack down by up to 1 MB */
+#define VM_MAX_ADDRESS          ((vm_offset_t) 0xFFE00000)
 
 
 
-#endif	/* _MACH_I386_VM_PARAM_H_ */
+#endif /* defined (__i386__) || defined (__x86_64__) */
+
+#endif  /* _MACH_I386_VM_PARAM_H_ */
