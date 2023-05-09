6a696769cff7af7de2a74676146d85bbef972083
Zequan Wu <zequanwu@google.com>
2023-05-09 15:04:54 -0400

    Refactor split dwarf handling.
    When we have skeleton compilation unit (DW_TAG_skeleton_unit) in a
    binary file refers to the complete unit in a split dwarf file
    (.dwo/.dwp file), we should use the split dwarf file's path in warning
    reporting. Right now, it uses the original file (binary file) path in
    warning report, which is incorrect.
    
    For example, if we have chrome.debug which is the binary with skeleton
    debug info and chrome.dwp which is the complete debug info and the debug
    info in chrome.dwp has some incorrect reference, it will warn on
    chrome.debug rather than chrome.dwp.
    
    This is also a preparation for a follow-up fix on relative offset being
    inserted into the global inline_origin_map, which causes keys collision.
    
    Bug: b/280290608
    Change-Id: I9874bbd215b0054771f4829253c0f195ac52a5ea

--- src/common/dwarf/dwarf2reader.h
+++ src/common/dwarf/dwarf2reader.h
@@ -479,7 +479,22 @@ class CompilationUnit {
   // headers. This plus the starting offset passed to the constructor
   // is the offset of the end of the compilation unit --- and the
   // start of the next compilation unit, if there is one.
-  uint64_t Start();
+  uint64_t Start(bool& ShouldProcessSplitDwarf);
+
+  // Process the actual debug information in a split DWARF file.
+  void ProcessSplitDwarf(std::string& split_file,
+                         SectionMap& sections,
+                         ByteReader& byte_reader);
+
+  const uint8_t* GetAddrBuffer() { return addr_buffer_; }
+
+  uint64_t GetAddrBufferLen() { return addr_buffer_length_; }
+
+  uint64_t GetAddrBase() { return addr_base_; }
+
+  uint64_t GetRangeBase() { return ranges_base_; }
+
+  uint64_t GetDWOID() { return dwo_id_; }
 
  private:
 
@@ -647,9 +662,6 @@ class CompilationUnit {
   // new place to position the stream to.
   const uint8_t* SkipAttribute(const uint8_t* start, enum DwarfForm form);
 
-  // Process the actual debug information in a split DWARF file.
-  void ProcessSplitDwarf();
-
   // Read the debug sections from a .dwo file.
   void ReadDebugSectionsFromDwo(ElfReader* elf_reader,
                                 SectionMap* sections);
@@ -743,11 +755,8 @@ class CompilationUnit {
   // True if we have already looked for a .dwp file.
   bool have_checked_for_dwp_;
 
-  // Path to the .dwp file.
-  string dwp_path_;
-
-  // ByteReader for the DWP file.
-  std::unique_ptr<ByteReader> dwp_byte_reader_;
+  // ElfReader for the dwo/dwo file.
+  std::unique_ptr<ElfReader> split_elf_reader_;
 
   // DWP reader.
    std::unique_ptr<DwpReader> dwp_reader_;
@@ -769,8 +778,6 @@ class CompilationUnit {
 class DwpReader {
  public:
   DwpReader(const ByteReader& byte_reader, ElfReader* elf_reader);
-
-  ~DwpReader();
 
   // Read the CU index and initialize data members.
   void Initialize();
