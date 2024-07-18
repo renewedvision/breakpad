fb2ae20caf4e98ae20c3f6b9850ad9d0f5a4b7e8
Greg Thompson <grt@chromium.org>
2024-07-18 09:22:54 +0200

    [windows] Include-what-you-use fixes in http_upload.{cc,h}
    Bug: b/353869880
    Change-Id: I7dd2d432032099c77410ce67a0fce6c7a71b473f

--- src/common/windows/http_upload.cc
+++ src/common/windows/http_upload.cc
@@ -31,6 +31,8 @@
 #endif
 
 #include <algorithm>
+#include <map>
+#include <memory>
 
 #include <assert.h>
 #include <stdint.h>
@@ -53,7 +55,6 @@ namespace {
   using std::string;
   using std::wstring;
   using std::map;
-  using std::ios;
   using std::unique_ptr;
 
 // Compresses the contents of `data` into `deflated` using the deflate
